9aef8a83d39bd42a1b5bde702ed1362d7ac89f8c
GeordiD <geordi.dosher@gmail.com>
2024-07-16 20:42:10 -0500

    nav bar

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const router = useRouter();
+const route = useRoute();
+
+const shouldShowBackButton = computed(() => route.path !== '/')
+</script>
+
+<template>
+  <div class="h-12 w-full bg-slate-700 text-white flex items-center px-2">
+    <div
+      v-if="shouldShowBackButton"
+      class="flex cursor-pointer hover:underline"
+      @click="router.push('/')"
+    >
+      <div class="mr-4">
+        <Icon
+          name="material-symbols:arrow-back"
+          size="1.5rem"
+        />
+      </div>
+      <div class="">
+        Teams
+      </div>
+    </div>
+    <div class="mx-auto">
+      NBA War Chest
+    </div>
+  </div>
+</template>
