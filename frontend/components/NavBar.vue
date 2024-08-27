1c067aa87feec2703cafed1cbac1a6f19e84fc61
GeordiD <geordi.dosher@gmail.com>
2024-08-27 09:07:25 -0500

    No more back button in navbar

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -1,32 +1,13 @@
 <script setup lang="ts">
-const router = useRouter();
-const route = useRoute();
-
-const shouldShowBackButton = computed(() => route.path !== '/')
 </script>
 
 <template>
   <div class="h-12 w-full bg-slate-700 text-white flex items-center px-2">
-    <div
-      v-if="shouldShowBackButton"
-      class="flex cursor-pointer hover:underline"
-      @click="router.push('/')"
-    >
-      <div class="mr-4">
-        <Icon
-          name="material-symbols:arrow-back"
-          size="1.5rem"
-        />
-      </div>
-      <div class="">
-        Teams
-      </div>
-    </div>
-    <div
+    <a
       class="mx-auto"
-      @click="router.push('/')"
+      href="/"
     >
       NBA War Chest
-    </div>
+    </a>
   </div>
 </template>
